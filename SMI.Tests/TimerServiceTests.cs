5dee31718bca969a1b6bfdecf60ae32511c7f105
WindDYTING <monk50108@gmail.com>
2025-05-13 17:55:22 +0800

    add [tests] for timeserivce.

---
+++ SMI.Tests/TimerServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using NSubstitute;
+
+namespace SMI.Tests;
+
+[TestFixture]
+public class TimerServiceTests
+{
+    TimerService _timerService;
+
+    [TearDown]
+    public void TearDown()
+    {
+        _timerService.Dispose();
+    }
+
+    [Test]
+    public void TimerService_SetTimeIsUpMoreDateTimeNow_CanTrigger()
+    {
+        var time = DateTime.Now.AddSeconds(1);
+        var action = Substitute.For<IReceivedAction>();
+        _timerService = new TimerService(time.ToTimeOnly());
+        _timerService.SetCheckInterval(0.5);
+        _timerService.TimeIsUp += (_, e) =>
+        {
+            action.Trigger();
+        };
+
+        Task.Delay(3000).Wait();
+
+        action.Received(1).Trigger();
+    }
+
+    [Test]
+    public void TimerService_SetTimeIsUpLessDateTimeNow_NotTrigger()
+    {
+        var time = DateTime.Now.Subtract(TimeSpan.FromSeconds(2));
+        var action = Substitute.For<IReceivedAction>();
+        _timerService = new TimerService(time.ToTimeOnly());
+        _timerService.SetCheckInterval(0.5);
+        _timerService.TimeIsUp += (_, e) =>
+        {
+            action.Trigger();
+        };
+
+        Task.Delay(3000).Wait();
+
+        action.DidNotReceive();
+    }
+}
